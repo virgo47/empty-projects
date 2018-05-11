74b828349b910c5fc7dfb9a5eb524588e4e008a4
richter <richard.richter@posam.sk>
2018-05-11 13:43:42 +0200

    real greeter test

--- java-with-test-gw/src/test/java/com/virgo47/GreeterTest.java
+++ java-with-test-gw/src/test/java/com/virgo47/GreeterTest.java
@@ -1,11 +1,14 @@
 package com.virgo47;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GreeterTest {
 
 	@Test
 	public void patternWithoutNamePlaceholder() {
-		 throw new RuntimeException("fails!");
+		Greeter greeter = new Greeter("Ahoj, {name}!");
+
+		Assertions.assertEquals("Ahoj, Virgo!", greeter.greet("Virgo"));
 	}
 }
